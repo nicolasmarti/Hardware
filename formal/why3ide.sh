035d9dcf85e12f5bd255d5ca082c81e50a4d87f4
Nicolas Marti <nicolas.marti.japon@gmail.com>
2021-11-03 10:54:24 +0900

    completed 01, finished 02 and starting 03

--- formal/why3ide.sh
+++ formal/why3ide.sh
@@ -1,7 +1,4 @@
 #echo $@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
-DIR1=$DIR
-DIR2=$DIR/01
-DIR3=$DIR/02
 
-why3 ide -L $DIR1 -L $DIR2 -L $DIR3 $@
+why3 ide -L $DIR -L $DIR/01 -L $DIR/02 -L $DIR/03 $@
